ab00f6c08b5451c7e8a236b42ddaa3028c9b59fd
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-06-06 14:34:10 -0500

    Added localstorage and memory

--- templates/Landing.js
+++ templates/Landing.js
@@ -1,20 +1,37 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('button').disabled = true;
-    document.querySelector('form').onsubmit = () => {
-        return false
+    if (localStorage.getItem("username") === null){
+        document.querySelector('button').disabled = true;
+        document.querySelector('form').onsubmit = () => {
+            return false
+        }
+        document.querySelector('#DISP').onkeyup = () => {
+            if (document.querySelector('#DISP').value.length > 0) {
+                document.querySelector('button').disabled = false;
+            }
+            else {
+                document.querySelector('button').disabled = true;
+            }
+        }
+        document.querySelector('button').onclick = () => {
+            const username = document.querySelector('#DISP').value;
+            document.querySelector('#intro').innerHTML = `Username: ${username}`;
+            localStorage.setItem('username', username);
+            document.querySelector(".UsernameInput").style.opacity = 0;
+            document.querySelector(".UsernameInput").style.height = 0;
+            document.querySelector('body').className = "";
+            document.querySelector('.centered-content').className = ""
+            document.querySelector('.appear').style.opacity = 1;
+            return false
+        }
     }
-    document.querySelector('#DISP').onkeyup = () => {
-        if (document.querySelector('#DISP').value.length > 0)
-            document.querySelector('button').disabled = false;
-        else
-            document.querySelector('button').disabled = true;
-    
-    document.querySelector('button').onclick = () => {
-        const username = document.querySelector('#DISP').value;
-        document.querySelector('#intro').innerHTML = `Welcome ${username}`;
-        document.querySelector('#intro').style.opacity = 1;
-        return false
+    else {
+        const username = localStorage.getItem("username");
+        document.querySelector('#intro').innerHTML = `Username: ${username}`;
+        document.querySelector(".UsernameInput").style.opacity = 0;
+        document.querySelector(".UsernameInput").style.height = 0;
+        document.querySelector('body').className = "";
+        document.querySelector('.centered-content').className = ""
+        document.querySelector('.appear').style.opacity = 1;
     }
-    };
 })
 
