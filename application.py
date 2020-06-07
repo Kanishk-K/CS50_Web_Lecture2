856f855db3bff0494638524d0ae01b42c0159dfa
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-06-07 11:57:47 -0500

    Fixed form submission issue and added comments to better expla in the code. Furthermore, I added the required dependencies for the python application to the txt.

--- application.py
+++ application.py
@@ -15,49 +15,59 @@ AllChannelsId = {}
 
 @app.route("/")
 def index():
+    #Submit main page.
     return render_template("Landing.html")
 @app.route("/PostChannel", methods=["POST"])
 def convert():
+    #From the request get the Channel's Intended Name.
     NewChannel = request.form.get("ChannelName")
+    #If the name of the channel is already present within the list then return a fail message, otherwise add it to the lists.
     if NewChannel in AllChannelsName:
-        print("There was a conflict")
         return jsonify(success = False, message = "The channel already exists")
     else:
-        print("There was not a conflict")
+        #Add the channel's name to the list of already existing one.
         AllChannelsName.append(NewChannel)
+        #Associate the Channel's name with its location id.
         AllChannelsId[str(NewChannel)] = len(AllChannelsId)
+        #Add an array that represents the new channel which will contain its messages.
         AllChannels.append([])
         return jsonify(success = True, message = f"The channel {NewChannel} was created")
 @app.route("/AllChannels", methods=["POST"])
 def send():
+    #When asked about all present channels provide a list of them.
     return jsonify(AllChannelsName)
 @app.route("/GetHistory",methods=["POST"])
 def history():
+    #Get channel name from the request.
     channel = request.form.get("channel")
-    print(channel)
     if channel in AllChannelsId.keys():
+        #If the channel exists send all messages that were previously sent.
         i = AllChannelsId[channel]
-        print("Channel found in dict")
-        print(AllChannels[i])
         return jsonify(success = True, messages = AllChannels[i])
     else:
-        print("Channel not found in dict")
+        #If the channel is not found, inform the client that the channel isn't avaliable.
         return jsonify(success = False, messages = "The channel was removed or unreachable.")
 @socketio.on("ChatSent")
 def ChatSent(ChatMessage):
+    #Get information from the sent message.
     TTS = False
     channel = ChatMessage["channel"]
     username = ChatMessage["username"]
     message = ChatMessage["message"]
+    #Check if the /tts command is present within the first 5 letters.
     if "/tts" in message[:5]:
+        #If it is then delete the mention of the first /tts and enable the TTS flag.
         TTS = True
         message = message.replace("/tts","",1)
+    #Append the date that the message was sent to the reformatted message.
     TimeSent = date.today().strftime("%a %b %d %Y %I:%M %p")
     TotalMessage = f"({TimeSent}) ({username}): {message}"
     i = AllChannelsId[channel]
+    #If the length of messages stored on this server is already 100, delete the oldest message from storage and append the newest one.
     if len(AllChannels[i]) == 100:
         AllChannels[i].pop(0)
         AllChannels[i].append(TotalMessage)
     else:
         AllChannels[i].append(TotalMessage)
+    #Send data back to the client, the new message, the channel it was sent to, the TTS flag, the original message (for TTS) and the user who sent it (for TTS).
     emit("ChatDistribute", {"RefMessage": TotalMessage, "channel" : channel, "TTS" : TTS, "OGmessage" : message, "username" : username}, broadcast=True)
--- static/script/Landing.js
+++ static/script/Landing.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+    //Initialize Variables to make the code neater.
     const InputButton = document.querySelector('#InputButton');
     const AddChannel = document.querySelector('#AddChannel');
     const FindChannel = document.querySelector('#FindChannel');
@@ -22,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const LeaveButton = document.querySelector("#LeaveButton");
     let GlobalChannel = null;
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-    
+    //Load avaliable voices, this will allow us to use the English locale.
     var available_voices = window.speechSynthesis.getVoices();
 	var english_voice = '';
 	for(var i=0; i<available_voices.length; i++) {
@@ -30,21 +31,17 @@ document.addEventListener('DOMContentLoaded', function(){
 			english_voice = available_voices[i];
 			break;
 		}
-	}
+    }
+    //On the chance that English is not avaliable, choose whatever locale is present.
 	if(english_voice === '')
-		english_voice = available_voices[0];
+        english_voice = available_voices[0];
+    //If the user has not logged on and was not present in a channel, present the display name form.
     if (localStorage.getItem("username") === null && localStorage.getItem("LastChannel") === null){
         InputButton.disabled = true;
         AddChannel.disabled = true;
         FindChannel.disabled = true;
         ChannelButton.disabled = true;
         ChatButton.disabled = true;
-        UsernameForm.onsubmit = () => {
-            return false
-        }
-        ChannelForm.onsubmit = () => {
-            return false
-        }
         UsernameInput.onkeyup = () => {
             if (UsernameInput.value.length > 0) {
                 InputButton.disabled = false;
@@ -64,22 +61,26 @@ document.addEventListener('DOMContentLoaded', function(){
             return false
         }
     }
+    //If the user does have a username but did not leave while connected to a channel allow the user to create/join a channel.
     else if (localStorage.getItem("username") !== null && localStorage.getItem("LastChannel") === null) {
         const username = localStorage.getItem("username");
         document.querySelector('#intro').innerHTML = `${username}`;
         UsernameForm.style.opacity = 0;
         UsernameForm.style.height = 0;
     }
+    //If the user left while connected to a channel and has a username, attempt to take them back to their respective connection.
     else {
         const username = localStorage.getItem("username");
         document.querySelector('#intro').innerHTML = `${username}`;
         UsernameForm.style.opacity = 0;
         UsernameForm.style.height = 0;
         GlobalChannel = localStorage.getItem("LastChannel");
+        //Fetch the previous history for the channel they were in.
         const request = new XMLHttpRequest();
         request.open('POST', '/GetHistory');
         request.onload = () => {
             const response = JSON.parse(request.responseText);
+            //If the server responds saying the channel exists, fetch previous messages. Otherwise, bring user to create/join channel page.
             if (response.success) {
                 console.log(response.messages);
                 response.messages.forEach((item, index) => {
@@ -110,6 +111,7 @@ document.addEventListener('DOMContentLoaded', function(){
         AddChannel.disabled = true;
         FindChannel.disabled = true;
     }
+    //When the user clicks the add channel button, bring up the add channel UI.
     AddChannel.onclick = () => {
         ChannelForm.style.opacity = 1;
         ChannelForm.style.height = "100%";
@@ -118,6 +120,7 @@ document.addEventListener('DOMContentLoaded', function(){
         ChatHolder.style.opacity = 0;
         ChatHolder.style.height = 0;
     }
+    //Ensure no empty submissions.
     ChannelInput.onkeyup = () => {
         if (ChannelInput.value.length > 0) {
             ChannelButton.disabled = false;
@@ -126,6 +129,7 @@ document.addEventListener('DOMContentLoaded', function(){
             ChannelButton.disabled = true;
         }
     }
+    //When the create channel button is pressed, send the name of the intended channel and clear the input field.
     ChannelButton.onclick = () => {
         const request = new XMLHttpRequest();
         const NewChannel = ChannelInput.value;
@@ -134,12 +138,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
         request.onload = () => {
             const response = JSON.parse(request.responseText);
-            
+            //If the server responds that the channel has been created, display that in a green notification.
             if (response.success) {
                 ChannelAlert.className = "alert alert-success ChannelAlert";
                 ChannelAlert.innerHTML = response.message;
 
             }
+            //If the server rejects the name then display that in a red notification.
             else {
                 ChannelAlert.className = "alert alert-danger ChannelAlert";
                 ChannelAlert.innerHTML = response.message;
@@ -151,6 +156,7 @@ document.addEventListener('DOMContentLoaded', function(){
         return false
     }
     FindChannel.onclick = () => {
+        //When the Find Channel button is clicked display the dropdown select UI.
         ChannelForm.style.opacity = 0;
         ChannelForm.style.height = 0;
         ChatHolder.style.opacity = 0;
@@ -158,10 +164,12 @@ document.addEventListener('DOMContentLoaded', function(){
         FindHolder.style.opacity = 1;
         FindHolder.style.height = "100%";
         ChannelList.innerHTML = "";
+        //Drop all previously held options on the dropdown before adding new options.
         document.querySelectorAll('#Dropdown option').forEach(option => option.remove())
         for (i = Dropdown.options.length-1; i>= 0; i--) {
             Dropdown.remove(i);
         }
+        //Ask the server for a list of all avaliable channels.
         const request = new XMLHttpRequest();
         request.open('POST', '/AllChannels');
 
@@ -179,6 +187,7 @@ document.addEventListener('DOMContentLoaded', function(){
         const AllChannels = new FormData();
         request.send(AllChannels);
     }
+    //When an item is selected from the dropdown.
     function onInput(e) {
         var input = e.target,
             val = input.value;
@@ -187,8 +196,7 @@ document.addEventListener('DOMContentLoaded', function(){
      
        for(var i = 0; i < options.length; i++) {
          if(options[i].innerText === val) {
-           // An item was selected from the list!
-           // yourCallbackHere()
+            //Something was selected, find what it was and assign it to val.
            AddChannel.disabled = true;
            FindChannel.disabled = true;
            FindHolder.style.opacity = 0;
@@ -196,11 +204,13 @@ document.addEventListener('DOMContentLoaded', function(){
            ChatHolder.style.opacity = 1;
            ChatHolder.style.height = "100%";
            GlobalChannel = val;
+           //Set the user's connected channel to the one they selected and get previous posts from the server.
            localStorage.setItem('LastChannel',val);
            ConnectedTo.innerHTML = `Connected To: ${val}`;
            const request = new XMLHttpRequest();
            request.open('POST', '/GetHistory');
            request.onload = () => {
+               //Get all previous messages and append them to the chat list.
                const response = JSON.parse(request.responseText);
                console.log(response);
                response.messages.forEach((item, index) => {
@@ -218,6 +228,7 @@ document.addEventListener('DOMContentLoaded', function(){
      }
     ChannelList.addEventListener('input', onInput);
     ChatInput.onkeyup = () => {
+        //Ensure no empty strings go through.
         if (ChatInput.value.length > 0) {
             ChatButton.disabled = false;
         }
@@ -227,17 +238,21 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     socket.on('connect', () => {
         ChatButton.onclick = () => {
+            //When a message is sent provide the server with the Channel it was sent to, the person who sent it and the message itself.
             const ChatMessage = ChatInput.value;
             ChatInput.value = "";
             socket.emit('ChatSent', {'channel':GlobalChannel,'username':document.querySelector('#intro').innerHTML,'message':ChatMessage});
         }
     })
     socket.on('ChatDistribute', message => {
+        //Check if the message that was sent from the server applies to the active channel.
         if (message.channel === ConnectedTo.innerHTML.substring(14,ConnectedTo.innerHTML.length)){
+            //If it does apply then add the previous messages to the chat list.
             var li = document.createElement("li");
             console.log(message.RefMessage);
             li.innerHTML = `${message.RefMessage}`;
             ChatBox.appendChild(li);
+            //If the server tells the client that the messages are TTS then use TTS to display them. (TTS = Text to Speech.)
             if (message.TTS === true){
                 var utter = new SpeechSynthesisUtterance();
                 utter.rate = 1;
@@ -249,6 +264,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     })
     LeaveButton.onclick = () => {
+        //Clear the LastChannel in local storage, remove the chat UI and remove the messages on the chat box on the client's side.
         localStorage.setItem("LastChannel",null);
         GlobalChannel = null;
         ConnectedTo.innerHTML = "";
